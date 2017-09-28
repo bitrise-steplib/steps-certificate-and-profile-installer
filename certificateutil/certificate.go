472487c7fe5477bb50f0bd4e35f6997e69a95305
trapacska <trapacska@gmail.com>
2017-09-28 14:51:35 +0200

    PR changes

--- certificateutil/certificate.go
+++ certificateutil/certificate.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bitrise-io/go-utils/fileutil"
+	"github.com/bitrise-io/go-utils/log"
+
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/pathutil"
 )
@@ -39,16 +42,7 @@ func convertP12ToPem(p12Pth, password string) (string, error) {
 	return pemPth, nil
 }
 
-func certificateInfosByPath(pemPth string) (CertificateInfosModel, error) {
-	out, err := command.New("openssl", "x509", "-in", pemPth, "-noout", "-enddate", "-subject").RunAndReturnTrimmedCombinedOutput()
-	if err != nil {
-		return CertificateInfosModel{}, fmt.Errorf("failed to read certificate infos, out: %s, error: %s", out, err)
-	}
-
-	return parseCertificateInfoOutput(out)
-}
-
-func certificateInfosByContent(pemContent []byte) (CertificateInfosModel, error) {
+func certificateInfos(pemContent []byte) (CertificateInfosModel, error) {
 	cmd := command.New("openssl", "x509", "-noout", "-enddate", "-subject")
 	cmd.SetStdin(bytes.NewReader(pemContent))
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
@@ -56,10 +50,6 @@ func certificateInfosByContent(pemContent []byte) (CertificateInfosModel, error)
 		return CertificateInfosModel{}, fmt.Errorf("failed to read certificate infos, out: %s, error: %s", out, err)
 	}
 
-	return parseCertificateInfoOutput(out)
-}
-
-func parseCertificateInfoOutput(out string) (CertificateInfosModel, error) {
 	lines := strings.Split(out, "\n")
 	if len(lines) < 2 {
 		return CertificateInfosModel{}, fmt.Errorf("failed to parse certificate infos")
@@ -69,22 +59,25 @@ func parseCertificateInfoOutput(out string) (CertificateInfosModel, error) {
 
 	// notAfter=Aug 15 14:15:19 2018 GMT
 	endDateLine := strings.TrimSpace(lines[0])
+	certificateInfos.RawEndDate = endDateLine
 	endDatePattern := `notAfter=(?P<date>.*)`
-	endDareRe := regexp.MustCompile(endDatePattern)
-	if matches := endDareRe.FindStringSubmatch(endDateLine); len(matches) == 2 {
+	endDateRe := regexp.MustCompile(endDatePattern)
+	if matches := endDateRe.FindStringSubmatch(endDateLine); len(matches) == 2 {
 		endDateStr := matches[1]
 		endDate, err := time.Parse("Jan 2 15:04:05 2006 MST", endDateStr)
-		if err != nil {
-			return CertificateInfosModel{}, fmt.Errorf("Failed to parse certificate end date, error: %s", err)
+		if err == nil {
+			certificateInfos.EndDate = endDate
+		} else {
+			log.Warnf("Failed to parse certificate endDate, error: %s", err)
 		}
-
-		certificateInfos.EndDate = endDate
 	} else {
-		certificateInfos.RawEndDate = endDateLine
+		log.Warnf("Failed to find pattern in %s, matches: %d", endDateLine, len(matches))
 	}
 
 	// subject= /UID=5KN/CN=iPhone Developer: Bitrise Bot (T36)/OU=339/O=Bitrise Bot/C=US
 	subjectLine := strings.TrimSpace(lines[1])
+	certificateInfos.RawSubject = subjectLine
+	certificateInfos.IsDevelopement = (strings.Contains(subjectLine, "Developer:") || strings.Contains(subjectLine, "Development:"))
 	subjectPattern := `subject= /UID=(?P<userID>.*)/CN=(?P<commonName>.*)/OU=(?P<teamID>.*)/O=(?P<name>.*)/C=(?P<local>.*)`
 	subjectRe := regexp.MustCompile(subjectPattern)
 	if matches := subjectRe.FindStringSubmatch(subjectLine); len(matches) == 6 {
@@ -101,8 +94,7 @@ func parseCertificateInfoOutput(out string) (CertificateInfosModel, error) {
 		certificateInfos.Local = local
 		certificateInfos.IsDevelopement = (strings.Contains(commonName, "Developer:") || strings.Contains(commonName, "Development:"))
 	} else {
-		certificateInfos.IsDevelopement = (strings.Contains(subjectLine, "Developer:") || strings.Contains(subjectLine, "Development:"))
-		certificateInfos.RawSubject = subjectLine
+		log.Warnf("Failed to find pattern in %s, matches: %d", subjectLine, len(matches))
 	}
 
 	return certificateInfos, nil
@@ -115,18 +107,23 @@ func CertificateInfosFromP12(p12Pth, password string) (CertificateInfosModel, er
 		return CertificateInfosModel{}, err
 	}
 
-	return certificateInfosByPath(pemPth)
+	pemContent, err := fileutil.ReadBytesFromFile(pemPth)
+	if err != nil {
+		return CertificateInfosModel{}, err
+	}
+
+	return certificateInfos(pemContent)
 }
 
 // CertificateInfosFromPemContent ...
 func CertificateInfosFromPemContent(pemContent []byte) (CertificateInfosModel, error) {
-	return certificateInfosByContent(pemContent)
+	return certificateInfos(pemContent)
 }
 
 func (certInfo CertificateInfosModel) String() string {
 	certInfoString := ""
 
-	if certInfo.RawSubject == "" {
+	if certInfo.CommonName != "" && certInfo.TeamID != "" {
 		certInfoString += fmt.Sprintf("- UserID: %s\n", certInfo.UserID)
 		certInfoString += fmt.Sprintf("- CommonName: %s\n", certInfo.CommonName)
 		certInfoString += fmt.Sprintf("- TeamID: %s\n", certInfo.TeamID)
@@ -136,7 +133,7 @@ func (certInfo CertificateInfosModel) String() string {
 		certInfoString += fmt.Sprintf("- RawSubject: %s\n", certInfo.RawSubject)
 	}
 
-	if certInfo.RawEndDate == "" {
+	if !certInfo.EndDate.IsZero() {
 		certInfoString += fmt.Sprintf("- EndDate: %s\n", certInfo.EndDate)
 	} else {
 		certInfoString += fmt.Sprintf("- RawEndDate: %s\n", certInfo.RawEndDate)
